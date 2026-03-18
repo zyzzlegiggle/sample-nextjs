36ca5a12378e7393f56cd1211395d19bc2450f41
jossealxh <112403713+zyzzlegiggle@users.noreply.github.com>
2026-03-19 01:26:35 +0800

    lightly: change title to my app

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
       {/* left side */}
       <div className="lg:w-1/2 relative z-10 flex flex-col justify-center px-10 lg:px-20 py-20 lg:py-0 text-left">
         <h2 className="inter text-4xl mb-3 font-bold text-gray-800">
-          Welcome to My Next.js App{" "}
+          my app
           <span className="block text-blue-500 text-2xl font-normal">
             Deployed to DigitalOcean
           </span>
